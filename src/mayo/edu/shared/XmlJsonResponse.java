06abec9718aa76bc1e6515e427a4566f69837274
cendle <endle.cory@mayo.edu>
2013-10-03 08:00:14 -0500

    Removed serialVersionUID.

--- src/mayo/edu/shared/XmlJsonResponse.java
+++ src/mayo/edu/shared/XmlJsonResponse.java
@@ -3,8 +3,6 @@ package mayo.edu.shared;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class XmlJsonResponse implements IsSerializable {
-
-	private static final long serialVersionUID = 2L;
 	
 	String i_json;
 	String i_xml;
